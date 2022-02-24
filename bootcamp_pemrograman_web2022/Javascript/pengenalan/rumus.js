16465d1037d284d464d7ecf613d3dedc2636f69a
Riki-Toro <rikitoro22@gmail.com>
2022-02-24 18:24:10 +0700

    pengenalan javascript

---
+++ bootcamp_pemrograman_web2022/Javascript/pengenalan/rumus.js
@@ -0,0 +1,7 @@
+function luasSegitiga() {
+    console.log('Rumus luas segitiga adalah 1/2 x alas x tinggi');
+}
+
+function luasPersegi() {
+    console.log('Rumus luas persegi adalah sisi x sisi');
+}
